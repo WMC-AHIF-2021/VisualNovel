cfd46f0870c0ee9d4a19fb8cf66b2736f4e5323b
LisaPich <pichlerlisa434@gmail.com>
2022-02-05 14:15:50 +0100

    added loadingscreen

---
+++ src/js/loadingscreen.ts
@@ -0,0 +1,22 @@
+async function wait(delay: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+function setDisplay(element: HTMLElement, display: string): void {
+    element.style.display = display;
+}
+
+setDisplay(
+    document.getElementById('main-content'), 'none'
+);
+
+document.addEventListener('DOMContentLoaded' , () =>
+wait(1500).then(() => {
+    setDisplay(
+        document.getElementById('main-content'), 'block'
+    );
+    
+    setDisplay(
+        document.getElementById('loadingscreen'), 'none'
+    );
+}));
